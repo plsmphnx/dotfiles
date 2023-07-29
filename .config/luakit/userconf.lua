2ca7c5075dd19828af19d6404d874136209c1ede
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2023-07-28 18:56:52 -0700

    More UX configs.

---
+++ .config/luakit/userconf.lua
@@ -0,0 +1,12 @@
+local settings = require "settings"
+
+settings.application.prefer_dark_mode = true
+
+settings.webview.enable_developer_extras = true
+settings.webview.enable_java = false
+settings.webview.enable_smooth_scrolling = true
+
+settings.window.close_with_last_tab = true
+settings.window.home_page = "google.com"
+settings.window.search_engines.nixpkgs = "https://search.nixos.org/packages?channel=unstable&query=%s"
+settings.window.search_engines.nixopts = "https://search.nixos.org/options?channel=unstable&query=%s"
