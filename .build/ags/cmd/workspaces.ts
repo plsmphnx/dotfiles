814f57714cd8ae3287e7a4cdb87d25974ee6afca
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2024-03-13 20:46:17 -0700

    Better multimonitor handling.

--- .build/ags/cmd/workspaces.ts
+++ .build/ags/cmd/workspaces.ts
@@ -9,31 +9,46 @@ async function exec(to: number, target?: string) {
 }
 
 function status() {
+    const active = hyprland.active.workspace.id;
     const monitor = hyprland.active.monitor.name;
+    const workspaces = hyprland.workspaces
+        .filter(w => w.id > 0)
+        .sort((a, b) => a.id - b.id);
     return {
-        active: hyprland.active.workspace.id,
-        monitor: hyprland.workspaces.filter(
-            w => w.id > 0 && w.monitor === monitor,
-        ),
+        active: workspaces.find(w => w.id === active)!,
+        monitor: workspaces.filter(w => w.monitor === monitor),
+        workspaces,
     };
 }
 
 async function prev(target?: string) {
-    const { active, monitor } = status();
-    const to = monitor.reverse().find(w => w.id < active)?.id || active - 1;
-    await exec(to, target);
+    const { active, monitor, workspaces } = status();
+    if (active.id !== monitor[0].id) {
+        const to = monitor.reverse().find(w => w.id < active.id)!.id;
+        await exec(to, target);
+    } else if (active.windows !== 0) {
+        const to = workspaces[0].id - 1;
+        await exec(to, target);
+    }
 }
 
 async function next(target?: string) {
-    const { active, monitor } = status();
-    const to = monitor.find(w => w.id > active)?.id || active + 1;
-    await exec(to, target);
+    const { active, monitor, workspaces } = status();
+    if (active.id !== monitor[monitor.length - 1].id) {
+        const to = monitor.find(w => w.id > active.id)!.id;
+        await exec(to, target);
+    } else if (active.windows !== 0) {
+        const to = workspaces[workspaces.length - 1].id + 1;
+        await exec(to, target);
+    }
 }
 
 async function empty(target?: string) {
-    const { monitor } = status();
-    const to = monitor[monitor.length - 1].id + 1;
-    await exec(to, target);
+    const { active, workspaces } = status();
+    const last = workspaces[workspaces.length - 1];
+    if (active.id !== last.id || last.windows !== 0) {
+        await exec(last.id + 1, target);
+    }
 }
 
 export default { prev, next, empty };
