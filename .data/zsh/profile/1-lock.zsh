28241924e38168b0d96d5f2ee49e53a956992534
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2024-09-03 15:37:28 -0700

    Switch back to exec-once.

--- .data/zsh/profile/1-lock.zsh
+++ .data/zsh/profile/1-lock.zsh
@@ -1,4 +1,4 @@
 if [ "$(tty)" = "/dev/tty1" ]; then
     exec systemd-cat -t hyprland hyprland \
-        -c "$XDG_CONFIG_HOME/hypr/lock/land.conf"
+        -c "$XDG_CONFIG_HOME/hypr/land/lock.conf"
 fi
--- .data/zsh/profile/3-hyprland.zsh
+++ .data/zsh/profile/3-hyprland.zsh
@@ -1,4 +1,4 @@
 if [ "$(tty)" = "/dev/tty3" ]; then
     exec systemd-cat -t hyprland hyprland \
-        -c "$XDG_CONFIG_HOME/hypr/desk/land.conf"
+        -c "$XDG_CONFIG_HOME/hypr/land/desk.conf"
 fi
