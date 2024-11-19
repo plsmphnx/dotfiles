f46d3a2a14d44a7b7247f19a3a20552e690c0d66
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2024-11-19 12:05:09 -0800

    Move shell to separate repo

--- .config/ags/lib/reset.ts
+++
@@ -1,7 +0,0 @@
-import { type Binding, Variable } from 'astal';
-
-export default (reset?: Binding<boolean>) => {
-    const toggle = Variable(false);
-    reset?.subscribe(v => v || toggle.set(false))
-    return toggle;
-};
--- .config/ags/lib/widgets.ts
+++
@@ -1,9 +0,0 @@
-import GObject from 'gi://GObject';
-
-import { astalify, Gtk } from 'astal/gtk3';
-
-export class Calendar extends astalify(Gtk.Calendar) {
-    static {
-        GObject.registerClass(this);
-    }
-}
