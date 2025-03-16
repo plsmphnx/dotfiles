d0686c48f77a73518331ce9f5a125ac371684cc9
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2025-03-16 16:59:04 -0700

    bat for man

--- .data/zsh/rc/color.zsh
+++ .data/zsh/rc/color.zsh
@@ -3,7 +3,5 @@ alias ls="ls --color -C"
 
 [[ -s "/etc/grc.zsh" ]] && source /etc/grc.zsh
 [[ -s "/usr/share/grc/grc.zsh" ]] && source /usr/share/grc/grc.zsh
-zcomet load ael-code/zsh-colored-man-pages
 
-# grc env interferes with man
-unset -f env
+man() { command man "$@" | bat -pl man ;}
