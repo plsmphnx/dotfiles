9aee09efb355300601cbfd0a2982ff1ae7fcc1de
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2025-05-13 15:38:13 -0700

    misc fixes

--- .data/zsh/rc/node.zsh
+++ .data/zsh/rc/node.zsh
@@ -1,2 +1,6 @@
+export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/config
+export NPM_CONFIG_CACHE=$XDG_CACHE_HOME/npm
+export NPM_CONFIG_TMP=$XDG_RUNTIME_DIR/npm
+
 path+=(./node_modules/.bin ~/.node_modules/bin)
 zcomet load lukechilds/zsh-better-npm-completion
