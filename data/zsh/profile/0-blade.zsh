e00fe3c2b5757b53f05af0894c0d43e6f9704433
Carl LeCompte <clecompt@microsoft.com>
2025-06-26 11:08:27 -0700

    keyring to gcr

--- data/zsh/profile/0-blade.zsh
+++ data/zsh/profile/0-blade.zsh
@@ -1,4 +1,3 @@
 INTEL=$(readlink -f /dev/dri/by-path/pci-0000:00:02.0-card)
 NVIDIA=$(readlink -f /dev/dri/by-path/pci-0000:01:00.0-card)
-export AQ_DRM_DEVICES="$NVIDIA:$INTEL"
-systemctl --user import-environment AQ_DRM_DEVICES
+systemctl --user set-environment AQ_DRM_DEVICES=$NVIDIA:$INTEL
