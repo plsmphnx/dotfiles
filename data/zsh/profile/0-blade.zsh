f493c61f6852f5741a24368d69ca229aabfae728
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2025-08-12 00:55:30 -0700

    fix shell on blade

--- data/zsh/profile/0-blade.zsh
+++ data/zsh/profile/0-blade.zsh
@@ -1,3 +1,5 @@
 INTEL=$(readlink -f /dev/dri/by-path/pci-0000:00:02.0-card)
 NVIDIA=$(readlink -f /dev/dri/by-path/pci-0000:01:00.0-card)
-systemctl --user set-environment AQ_DRM_DEVICES=$NVIDIA:$INTEL
+systemctl --user set-environment    \
+    AQ_DRM_DEVICES=$NVIDIA:$INTEL   \
+    MESA_VK_DEVICE_SELECT=10de:249c \
