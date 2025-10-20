18ca387b87678ef261933e3a1d0a1e1df1701786
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2025-10-20 00:51:19 -0700

    lite-xl -> pragtical

--- config/ux/pragtical/plugins/file.lua
+++ config/ux/pragtical/plugins/file.lua
@@ -1,3 +1,4 @@
+-- mod-version:3
 local command  = require 'core.command'
 local treeview = require 'plugins.treeview'
 
@@ -6,5 +7,7 @@ if #ARGS < 2 then
 end
 
 if #ARGS < 2 or system.get_file_info(ARGS[2]).type ~= "dir" then
+    command.perform "core:toggle-tabs"
+    command.perform "status-bar:toggle"
     command.perform "treeview:toggle"
 end
