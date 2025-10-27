cb4a580088394796618c826b0a45952cadc1b764
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2025-10-27 13:51:02 -0700

    dynamic lsp init

--- config/ux/pragtical/init.lua
+++ config/ux/pragtical/init.lua
@@ -45,3 +45,19 @@ else
         width = 1
     }
 end
+
+local lsp_servers = {
+    gopls = {},
+    rust_analyzer = { command = { "ra-multiplex" } },
+    tsserver = {}
+}
+local ok, lspconfig = pcall(require, "plugins.lsp.config")
+if ok then
+    local lsputil = require "plugins.lsp.util"
+    for s, c in pairs(lsp_servers) do
+        local cmd = c.command or lspconfig[s].get_options().command
+        if lsputil.command_exists(cmd[1]) then
+            lspconfig[s].setup(c)
+        end
+    end
+end
