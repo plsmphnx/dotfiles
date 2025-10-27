dd45ef8fd18117ad0a92b0e5e9e7c85a3634621b
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2025-10-27 14:17:18 -0700

    add language-specific env files

--- data/zsh/rc/node.zsh
+++ data/zsh/rc/node.zsh
@@ -1,6 +1,2 @@
-export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/config
-export NPM_CONFIG_CACHE=$XDG_CACHE_HOME/npm
-export NPM_CONFIG_TMP=$XDG_RUNTIME_DIR/npm
-
 path+=(./node_modules/.bin ~/.node_modules/bin)
 zcomet load lukechilds/zsh-better-npm-completion
