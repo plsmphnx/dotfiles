a1c88d1a932da1742df457b2b3d46750fb795da8
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2026-05-23 01:39:28 -0700

    cleanup

--- config/ux/hypr/util/bind.lua
+++ config/ux/hypr/util/bind.lua
@@ -29,7 +29,7 @@ local function keys(mods, key)
   if mods and mods ~= "" then return mods .. "+" .. key else return key end
 end
 
-return setmetatable({ next = {} }, {
+return setmetatable({ _ = {} }, {
   __call = function(self, binds)
     for _, v in pairs(binds) do
       for i, d in ipairs(v) do
@@ -37,46 +37,39 @@ return setmetatable({ next = {} }, {
       end
     end
 
-    local sub = rawget(self, "sub")
     local mods = {}
     local target = self
     while target do
-      local s = rawget(target, "sub")
       local m = keys(table.concat(mods, "+"), "")
       local build = function(binder)
         for k, v in pairs(binds) do binder(m .. k, v) end
 
         for _, mod in ipairs(mods) do
           for _, key in ipairs(MODS[mod]) do
-            hl.bind(m .. key, hl.dsp.submap(sub), { release = true })
+            hl.bind(m .. key, hl.dsp.submap(self._.sub), { release = true })
           end
         end
       end
 
-      if s then hl.define_submap(s, function()
+      if target._.sub then hl.define_submap(target._.sub, function()
         build(bind_exit)
       end) else build(bind) end
 
-      table.insert(mods, 1, rawget(target, "mod"))
-      target = rawget(target, "prev")
+      table.insert(mods, 1, target._.mod)
+      target = target._.prev
     end
   end,
 
   __index = function(self, key)
-    local next = rawget(self, "next")
-    local val = next[key]
-    if not val then
-      local mod = key:upper()
-      local sub = keys(rawget(self, "sub"), mod)
-      hl.define_submap(sub, function()
-        hl.bind("catchall", hl.dsp.submap "reset", { release = true })
-      end)
-      val = setmetatable(
-        { mod = mod, sub = sub, prev = self, next = {} },
-        getmetatable(self)
-      )
-      next[key] = val
-    end
+    local mod = key:upper()
+    local sub = keys(self._.sub, mod)
+    hl.define_submap(sub, function()
+      hl.bind("catchall", hl.dsp.submap "reset", { release = true })
+    end)
+    local val = setmetatable({
+      _ = { mod = mod, sub = sub, prev = self },
+    }, getmetatable(self))
+    self[key] = val
     return val
   end,
 })
--- config/ux/hypr/util/jump.lua
+++ config/ux/hypr/util/jump.lua
@@ -1,10 +1,9 @@
-local function jump(self)
+local function jump(spec, cmds)
   local active_monitor = hl.get_active_monitor()
   local workspaces = hl.get_workspaces()
   table.sort(workspaces, function(a, b) return a.id < b.id end)
 
-  local active
-  local monitor = {}
+  local monitor, active = {}
   for i, workspace in ipairs(workspaces) do
     if workspace.monitor == active_monitor then
       monitor[#monitor + 1] = i
@@ -15,53 +14,51 @@ local function jump(self)
   end
 
   local id
-  if self.prev then
-    if active == 1 or self.free then
+  if spec.prev then
+    if active == 1 or spec.free then
       local i = monitor[1]
       id = workspaces[i].id
-      if not self.used and workspaces[i].windows ~= 0 then
+      if not spec.used and workspaces[i].windows ~= 0 then
         while i > 0 and workspaces[i].id == id do
-          i = i - 1
-          id = id - 1
+          i, id = i - 1, id - 1
         end
       end
     else id = workspaces[monitor[active - 1]].id end
   else
-    if active == #monitor or self.free then
+    if active == #monitor or spec.free then
       local i = monitor[#monitor]
       id = workspaces[i].id
-      if not self.used and workspaces[i].windows ~= 0 then
+      if not spec.used and workspaces[i].windows ~= 0 then
         while i <= #workspaces and workspaces[i].id == id do
-          i = i + 1
-          id = id + 1
+          i, id = i + 1, id + 1
         end
       end
     else id = workspaces[monitor[active + 1]].id end
   end
   local workspace = tostring(math.max(1, math.min(2147483647, id)))
 
-  for _, cmd in ipairs(self) do
-    if type(cmd) == "function" then
-      local dispatcher = cmd { workspace = workspace }
-      if dispatcher then hl.dispatch(dispatcher) end
-    else hl.dispatch(cmd) end
+  for _, cmd in ipairs(cmds) do
+    if type(cmd) == "function" then cmd = cmd { workspace = workspace } end
+    if cmd then hl.dispatch(cmd) end
   end
 end
 
-return setmetatable({}, {
+function default(cmds)
+  for _, cmd in ipairs(cmds) do
+    if type(cmd) == "function" then return cmds end
+  end
+  return table.insert(cmds, 1, hl.dsp.focus) or cmds
+end
+
+return setmetatable({ _ = {} }, {
   __call = function(self, ...)
-    local cmds = {...}
-    local focus = true
-    for _, cmd in ipairs(cmds) do
-      if type(cmd) == "function" then focus = false end
-    end
-    if focus then table.insert(cmds, 1, hl.dsp.focus) end
-    for k, v in pairs(self) do cmds[k] = v end
-    return function() jump(cmds) end
+    local spec, cmds = self._, default {...}
+    return function() jump(spec, cmds) end
   end,
   __index = function(self, key)
-    local next = setmetatable({ [key] = true }, getmetatable(self))
-    for k, v in pairs(self) do next[k] = v end
-    return next
+    local val = setmetatable({ _ = { [key] = true } }, getmetatable(self))
+    for k in pairs(self._) do val._[k] = true end
+    self[key] = val
+    return val
   end,
 })
